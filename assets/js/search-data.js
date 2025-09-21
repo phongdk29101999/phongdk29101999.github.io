9b2e03b50cdebee0df01a7dcac1325f30ef10a72
phongdk29101999 <phongkimdo2910@gmail.com>
2025-09-21 02:55:41 +0000

    Deploying to gh-pages from @ phongdk29101999/phongdk29101999.github.io@9f35d60249f69f6d42ffc348f85e92ee569de382 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,8 +9,8 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-bloc",
-          title: "Bloc",
+  },{id: "nav-blog",
+          title: "Blog",
           description: "",
           section: "Navigation",
           handler: () => {
